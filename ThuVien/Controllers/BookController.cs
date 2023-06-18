b42811565eaafef0604d5a15f1c0dee4489b1bb4
Kháng Nguyễn <khangnguyen282002@gmail.com>
2023-06-18 13:52:01 +0700

    update-home

---
+++ ThuVien/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThuVien.Models;
+
+namespace ThuVien.Controllers
+{
+    public class BookController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            List<sach> sachList = db.saches.ToList();
+            return View(sachList);
+        }
+    }
+}
